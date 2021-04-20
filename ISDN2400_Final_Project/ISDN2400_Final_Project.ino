15b74cb73e122ee6ad7a1e66c59c08ab4a267f22
renaultluk <ren66.rl@gmail.com>
2021-04-20 18:42:36 +0800

    Update ISDN2400_Final_Project.ino

--- ISDN2400_Final_Project/ISDN2400_Final_Project.ino
+++ ISDN2400_Final_Project/ISDN2400_Final_Project.ino
@@ -4,7 +4,9 @@
 #define M2IN2       9
 
 float cruise_speed = 128.0;
+float turn_angle = 0.0;
 float turn_speed = 128.0;
+int[] LED_arr = [2,3,4,5];
 
 void bluetooth_parse(String input) {
   String command;
@@ -18,7 +20,7 @@ void bluetooth_parse(String input) {
   if (command == "cs") {
       cruise_speed = value;
   } else if (command == "an") {
-      
+      turn_angle = value;
   }
 }
 
@@ -32,39 +34,63 @@ void motorDrive(int power, int in1, int in2) {
     }
 }
 
-void forward() {
-    motorDrive(cruise_speed, M1IN1, M1IN2);
-    motorDrive(cruise_speed, M2IN1, M2IN2);    
+void update_speed(float V, float theta) {
+  float V_outer, V_inner;
+  float differential;
+
+  differential = V * sin((pi/2) - fabs(theta));
+  V_inner = differential;
+  V_outer = 2*V - differential;
+  
+  if (theta > 0) {
+    motorDrive(V_outer, M1IN1, M1IN2);
+    motorDrive(V_inner, M2IN1, M2IN2);
+  } else {
+    motorDrive(V_inner, M1IN1, M1IN2);
+    motorDrive(V_outer, M2IN1, M2IN2);
+  }
 }
 
-void swerve_right() {}
-
-void swerve_left() {}
-
-void turn_left() {
-    motorDrive(-turn_speed, M1IN1, M1IN2);
-    motorDrive(turn_speed, M2IN1, M2IN2);
+void state_update(float V, float theta) {
+  if (V == 0) {
+      for (int i=0; i<4; i++) {
+        digitalWrite(LED_arr[i], LOW);
+      }
+  } else {
+      if (fabs(theta) < 0.05) {
+        for (int i=0; i<4; i++) {
+          digitalWrite(LED_arr[i], HIGH);
+        }
+      } else {
+          if (theta < 0) {
+            digitalWrite(LED_arr[0], HIGH);
+            digitalWrite(LED_arr[1], HIGH);
+            digitalWrite(LED_arr[2], LOW);
+            digitalWrite(LED_arr[3], LOW);
+          } else {
+            digitalWrite(LED_arr[0], LOW);
+            digitalWrite(LED_arr[1], LOW);
+            digitalWrite(LED_arr[2], HIGH);
+            digitalWrite(LED_arr[3], HIGH);
+          }
+      }
+  }
 }
 
-void turn_right() {
-    motorDrive(turn_speed, M1IN1, M1IN2);
-    motorDrive(-turn_speed, M2IN1, M2IN2);
-}
-
-void halt() {
-    motorDrive(0, M1IN1, M1IN2);
-    motorDrive(0, M2IN1, M2IN2);
-}
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  motorDrive(128, M1IN1, M1IN2);
+  motorDrive(128, M2IN1, M2IN2);
+  delay(100000);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (Serial.available > 0) {
-     String data = Serial.read();
+  if (Serial.available() > 0) {
+     String data = Serial.readString();
      bluetooth_parse(data);
+     update_speed(cruise_speed, turn_angle);
   }
 }
