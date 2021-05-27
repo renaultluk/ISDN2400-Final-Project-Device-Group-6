daa66175f84762ebadbb72cb07b4e8536885fea6
renaultluk <ren66.rl@gmail.com>
2021-05-27 14:05:32 +0800

    Update ISDN2400_Final_Project.ino

--- ISDN2400_Final_Project/ISDN2400_Final_Project.ino
+++ ISDN2400_Final_Project/ISDN2400_Final_Project.ino
@@ -6,9 +6,9 @@
 #define M2IN2       3
 
 float cruise_speed = 128.0;
-float turn_angle = 0.0;
+float turn_angle = 60.0;
 float turn_speed = 128.0;
-int LED_arr[4] = {4,5,6,7};
+int LED_arr[4] = {8,5,6,7};
 
 void motorDrive(int power, int in1, int in2) {
   if (power >= 0) {
@@ -68,6 +68,11 @@ BLEService ledService("19B10010-E8F2-537E-4F6C-D104768A1214");
 BLEStringCharacteristic speedCharacteristic("19B10011-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite, 20);
 BLEStringCharacteristic angleCharacteristic("19B10012-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite, 20);
 
+void blePeripheralDisconnectHandler(BLEDevice central) {
+  motorDrive(0, M1IN1, M1IN2);
+  motorDrive(0, M2IN1, M2IN2);
+}
+
 void setup() {
   Serial.begin(9600);
   
@@ -87,23 +92,48 @@ void setup() {
 
   BLE.addService(ledService);
 
-  speedCharacteristic.writeValue("0");
-  angleCharacteristic.writeValue("0");
+  BLE.setEventHandler(BLEDisconnected, blePeripheralDisconnectHandler);
+
+  speedCharacteristic.writeValue("256");
+  angleCharacteristic.writeValue("60");
   
   BLE.advertise();
 
   Serial.println("Bluetooth device active, waiting for connections...");
   
-  motorDrive(128, M1IN1, M1IN2);
-  motorDrive(128, M2IN1, M2IN2);
+  motorDrive(0, M1IN1, M1IN2);
+  motorDrive(0, M2IN1, M2IN2);
 
   for (int i=0; i<4; i++) {
     pinMode(LED_arr[i], OUTPUT);
+  }
+
+  for (int i=0; i<4; i++) {
+    digitalWrite(LED_arr[i], HIGH);
+    delay(100);
+    digitalWrite(LED_arr[i], LOW);
+  }
+
+  for (int i=0; i<3; i++) {
+    digitalWrite(LED_arr[2-i], HIGH);
+    delay(100);
+    digitalWrite(LED_arr[2-i], LOW);
   }
 }
 
 void loop() {
   BLE.poll();
+
+  if (!BLE.connected()) {
+    for (int i=0; i<4; i++) {
+      digitalWrite(LED_arr[i], HIGH);
+    }
+    delay(500);
+    for (int i=0; i<4; i++) {
+      digitalWrite(LED_arr[i], LOW);
+    }
+    delay(500);
+  }
   
   if (speedCharacteristic.written() || angleCharacteristic.written()) {
     String tmpStr1 = speedCharacteristic.value();
